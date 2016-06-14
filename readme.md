fa3854d9302abc8bec8c6232f87a9f21d3283c87
Joe Niland <joe@originalmind.com.au>
2016-06-14 15:36:11 +1000

    initial version

---
+++ readme.md
@@ -0,0 +1,4 @@
+Provision-Salt :: BoxStarter package to install Salt on Windows
+----------------------------------------------------------------
+
+This BoxStarter packages is a very basic wrapper around bootstrap-salt.ps1. Requires further work to expose config variables or pull them from a config store, etc.
---
+++ tools/ChocolateyInstall.ps1
@@ -0,0 +1,21 @@
+try {
+
+	$SALT_VERSION = '2016.3.0'
+
+	$downloadDir = "${env:temp}\salt\"
+
+	if (!(Test-Path $downloadDir)) {
+		New-Item -Type Directory $downloadDir
+	}
+
+	(new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/saltstack/salt-bootstrap/stable/bootstrap-salt.ps1', "${downloadDir}\bootstrap-salt.ps1") | Out-Null
+
+	if (!(Test-Path "${downloadDir}\bootstrap-salt.ps1")) {
+		throw "There was a problem downloading the bootstrap-salt.ps1 file to ${downloadDir}"
+	}
+
+	Invoke-Expression "${env:temp}\salt\bootstrap-salt.ps1 -version $SALT_VERSION -runservice true -minion salt-minion -master `"master`""
+
+} catch {
+	throw $_.Exception
+}
